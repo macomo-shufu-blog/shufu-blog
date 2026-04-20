39a99daa737432e4fc69e83ba4e05f3eb48451e0
anna macomo <annamacomo@anna2024.local>
2026-04-20 10:22:08 +0900

    サイトタイトル・ナビ・フッターを日本語化

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'まこもノート';
+export const SITE_DESCRIPTION = '専業主婦3児ママの資産形成・日常・家計管理リアル日記';
